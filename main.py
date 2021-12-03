c2c4eb5615ef71ca904e5f2ce997e49f5df29323
Efrain Rosas <carlos.rosas7476@alumnos.udg.mx>
2021-12-02 23:23:09 -0600

    Cracion de función read para leer un archivo

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+"""
+Creamos una funcion read que nos permita leer "archivo.in" dicho archivo
+tiene en la primer linea el numero n de tarjetas y las siguientes n lineas
+los los valores de las tarjetas.
+La función read recibe como parametro el nombre del archivo y devuelve una lista
+con los valores de las tarjetas.
+"""
+def read(archivo):
+    lista_tarjetas = []
+    with open(archivo, "r", encoding="utf-8") as archivo:
+        n = int(archivo.readline())
+        for i in range(n):
+            lista_tarjetas.append(int(archivo.readline()))
+    return lista_tarjetas
+
+
 def main():
     pass
 
